f41706a02da83580fb522f928f66642530b565e7
Júlia Nogueira <69528739+juliaNogueiraC@users.noreply.github.com>
2020-11-29 13:40:47 -0300

    Create SecurityConfig.java

---
+++ src/main/java/resolucaoeskillo/com/demo/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package resolucaoeskillo.com.demo.config;
+
+import java.util.Arrays;
+
+import resolucaoeskillo.com.demo.security.JWTAuthenticationFilter;
+import resolucaoeskillo.com.demo.security.JWTAuthorizationFilter;
+import resolucaoeskillo.com.demo.security.JWTUtil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    private static final String[] URL_PUBLICAS = {
+        "/h2-console/**"
+    };
+
+    private static final String[] URL_PUBLICAS_GET = {
+        "/areas-de-estudo/**",
+        "/tipos-de-competencia/**",
+        "/tipos-de-formacao/**",
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.cors().and().csrf().disable();
+
+        http.authorizeRequests()
+            .antMatchers(URL_PUBLICAS).permitAll()
+            .antMatchers(HttpMethod.GET, URL_PUBLICAS_GET).permitAll()
+            .anyRequest().authenticated();
+
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+       
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
